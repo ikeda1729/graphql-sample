8d1bd92c8d061202c28bd68f0e7a861be2038070
ikeda <d.enkikairo@gmail.com>
2023-08-17 15:14:20 +0900

    book api

---
+++ app/repository/book_repo.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"my_gql_server/app/models"
+	"my_gql_server/graph/model"
+
+	"gorm.io/gorm"
+)
+
+type BookRepository interface {
+	CreateBook(bookInput *model.BookInput) (*models.Book, error)
+	UpdateBook(bookInput *model.BookInput, id int) error
+	DeleteBook(id int) error
+	GetOneBook(id int) (*models.Book, error)
+	GetAllBooks() ([]*model.Book, error)
+}
+
+type BookService struct {
+	Db *gorm.DB
+}
+
+var _ BookRepository = &BookService{}
+
+func NewBookService(db *gorm.DB) *BookService {
+	return &BookService{
+		Db: db,
+	}
+}
+
+func (b *BookService) CreateBook(bookInput *model.BookInput) (*models.Book, error) {
+	book := &models.Book{
+		Title:     bookInput.Title,
+		Author:    bookInput.Author,
+		Publisher: bookInput.Publisher,
+	}
+	err := b.Db.Create(&book).Error
+
+	return book, err
+}
+func (b *BookService) UpdateBook(bookInput *model.BookInput, id int) error {
+	book := models.Book{
+		ID:        id,
+		Title:     bookInput.Title,
+		Author:    bookInput.Author,
+		Publisher: bookInput.Publisher,
+	}
+	err := b.Db.Model(&book).Where("id = ?", id).Updates(book).Error
+	return err
+}
+
+func (b *BookService) DeleteBook(id int) error {
+	book := &models.Book{}
+	err := b.Db.Delete(book, id).Error
+	return err
+}
+
+func (b *BookService) GetOneBook(id int) (*models.Book, error) {
+	book := &models.Book{}
+	err := b.Db.Where("id = ?", id).First(book).Error
+	return book, err
+}
+
+func (b *BookService) GetAllBooks() ([]*model.Book, error) {
+	books := []*model.Book{}
+	err := b.Db.Find(&books).Error
+	return books, err
+
+}
---
+++ server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"my_gql_server/app/database"
+	"my_gql_server/app/repository"
+	"my_gql_server/graph"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
+
+	"gorm.io/gorm"
+)
+
+const defaultPort = "8080"
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	godotenv.Load()
+	config := &database.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+	db, err := database.NewConnection(config)
+	if err != nil {
+		panic(err)
+	}
+	database.Migrate(db)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+	repo := repository.NewBookService(db)
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{BookRepository: repo}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
